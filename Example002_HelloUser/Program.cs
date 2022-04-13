05e3d0f1eb0164ee011d27b06e5e22e316201b25
«Sergey <sdhomenko@gmail.com>
2022-04-13 22:44:37 +0300

    Example001_HelloConsole

--- Example002_HelloUser/Program.cs
+++ Example002_HelloUser/Program.cs
@@ -1,4 +1,4 @@
-﻿Console.Write("Enter your name: ");
-String username = Console.ReadLine();
+Console.Write("Enter your name: ");
+string username = Console.ReadLine();
 Console.Write ("YO!");
 Console.Write (username);
