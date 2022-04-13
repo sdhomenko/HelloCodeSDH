8831435e61feaa48ae1610d12ab64e65d6dc7d4c
«Sergey <sdhomenko@gmail.com>
2022-04-13 20:42:09 +0300

    Example005_ConditionIfElse

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Enter User Name: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "sergey")
+{
+    Console.WriteLine("YO! It`s The Best DJ CYRAX!");
+}
+else
+{
+    Console.Write("Hi! ");
+    Console.WriteLine(username);
+}
