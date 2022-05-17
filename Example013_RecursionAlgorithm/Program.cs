dfc266ed84311830acc1c2463d07ad65f392dd29
Sergey Homenko <sdhomenko@gmail.com>
2022-05-17 17:35:38 +0300

    Lection 6

---
+++ Example013_RecursionAlgorithm/Program.cs
@@ -0,0 +1,34 @@
+﻿string[,] table = new string[2, 5];
+
+table[1, 2] = "Hip-hop";
+
+
+void PrintArray(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillArray(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j]= new Random().Next(1, 10);
+        }
+    }
+}
+
+int[,] matrix = new int[3, 4];
+PrintArray(matrix);
+FillArray(matrix);
+Console.WriteLine();
+PrintArray(matrix);
+
